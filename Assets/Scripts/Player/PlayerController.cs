0279a466ac120903fc79d0177417f6acaef3aa34
Jack Malvey <john.malvey@mymail.champlain.edu>
2018-11-26 10:47:15 -0500

    added hand and began to add push animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,18 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 [RequireComponent(typeof(PlayerMotor))]
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	private float mSpeed = 5.0f;
-	private float mMouseSensitivity = 3.0f;
-	private PlayerMotor mMotor;
-	public bool mIsJumping = false;
-	public bool mIsDoubleJumpAvailable = true;
+    private float mSpeed = 5.0f;
+    private float mMouseSensitivity = 3.0f;
+    private PlayerMotor mMotor;
+    public bool mIsJumping = false;
+    public bool mIsDoubleJumpAvailable = true;
     public GameObject loseScreen;
+
+    bool beginPush = false;
+    bool endPush = false;
     bool canMove = true;
-	void Start () {
-		this.mMotor = GetComponent<PlayerMotor>();
-	}
+
+   
+    public GameObject handAnchor;
+    public GameObject handAnchorPush;
+    public GameObject hand;
+
+    void Start()
+    {
+        this.mMotor = GetComponent<PlayerMotor>();
+        Cursor.visible = false;
+    }
 
     void Update()
     {
@@ -24,59 +36,95 @@ public class PlayerController : MonoBehaviour {
             GetKeyboardInput();
             GetMouseInput();
         }
+        if (beginPush)
+            pushHand();
+        if (endPush)
+            pullHand();
     }
 
-	/* PRIVATE FUNCTIONS */
-	private void CheckState() {
-		if (mMotor.mIsGrounded == true) {
+    /* PRIVATE FUNCTIONS */
+    private void CheckState()
+    {
+        if (mMotor.mIsGrounded == true)
+        {
 
-			mIsJumping = false;
-			mIsDoubleJumpAvailable = true;
-		}
+            mIsJumping = false;
+            mIsDoubleJumpAvailable = true;
+        }
         if (mMotor.MIsDead == true)
         {
             canMove = false;
             loseScreen.SetActive(true);
         }
-	}
-	private void GetKeyboardInput() {
-		// Calculate movement velocit as a 3D vector for keyboard controls
-		float xMov = Input.GetAxisRaw("Horizontal");
-		float zMov = Input.GetAxisRaw("Vertical");
+    }
+    private void GetKeyboardInput()
+    {
+        // Calculate movement velocit as a 3D vector for keyboard controls
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
 
-		Vector3 movHorizontal = transform.right * xMov;
-		Vector3 movVertical = transform.forward * zMov;
+        Vector3 movHorizontal = transform.right * xMov;
+        Vector3 movVertical = transform.forward * zMov;
 
-		// Calculate final movement vector
-		Vector3 finalVel = (movHorizontal + movVertical).normalized * this.mSpeed;
+        // Calculate final movement vector
+        Vector3 finalVel = (movHorizontal + movVertical).normalized * this.mSpeed;
 
-		// Apply movement vector
-		mMotor.Move(finalVel);
+        // Apply movement vector
+        mMotor.Move(finalVel);
 
-		// Check if the player pressed the spacebar
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			if (mIsJumping == false) {
-				mMotor.Jump();
-				mIsJumping = true;
-			}
-			else if (mIsJumping == true && mIsDoubleJumpAvailable == true && mMotor.mIsGrounded == false) {
-				mMotor.Jump();
-				mIsDoubleJumpAvailable = false;
-			}
-		}		
-	}
+        // Check if the player pressed the spacebar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (mIsJumping == false)
+            {
+                mMotor.Jump();
+                mIsJumping = true;
+            }
+            else if (mIsJumping == true && mIsDoubleJumpAvailable == true && mMotor.mIsGrounded == false)
+            {
+                mMotor.Jump();
+                mIsDoubleJumpAvailable = false;
+            }
+        }
+    }
 
-	private void GetMouseInput() {
-		// Calculate rotation as a 3D vector for mouse controls
-		float yRot = Input.GetAxisRaw("Mouse X");
-		// Calculate final rotation vector
-		Vector3 rot = new Vector3(0f, yRot, 0f) * mMouseSensitivity;
-		// Apply rotation vector
-		mMotor.Rotate(rot);
+    private void GetMouseInput()
+    {
+        // Calculate rotation as a 3D vector for mouse controls
+        float yRot = Input.GetAxisRaw("Mouse X");
+        // Calculate final rotation vector
+        Vector3 rot = new Vector3(0f, yRot, 0f) * mMouseSensitivity;
+        // Apply rotation vector
+        mMotor.Rotate(rot);
 
-		// Calculate and apply camera rotation
-		float xRot = Input.GetAxisRaw("Mouse Y");
-		float camRot = xRot * mMouseSensitivity;
-		mMotor.RotateCamera(camRot);
-	}
+        // Calculate and apply camera rotation
+        float xRot = Input.GetAxisRaw("Mouse Y");
+        float camRot = xRot * mMouseSensitivity;
+        mMotor.RotateCamera(camRot);
+
+
+        if(Input.GetMouseButton(0) && !beginPush && !endPush)
+        {
+            beginPush = true;
+        }
+
+    }
+
+    private void pushHand()
+    {
+
+        hand.transform.position = Vector3.Lerp(hand.transform.position, handAnchorPush.transform.position,Time.deltaTime * 5);
+        if (hand.transform.position == handAnchorPush.transform.position)
+        {
+            beginPush = false;
+            endPush = true;
+        }
+    }
+    private void pullHand()
+    {
+        hand.transform.position = Vector3.Lerp(hand.transform.position, handAnchor.transform.position, Time.deltaTime * 5);
+        if (hand.transform.position == handAnchor.transform.position) 
+            endPush = false;
+        
+    }
 }

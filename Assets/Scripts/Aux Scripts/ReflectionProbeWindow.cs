308fd6efde30d299819e114f3b525fd3b61d29ec
Jack Malvey <john.malvey@mymail.champlain.edu>
2018-12-03 19:53:19 -0500

    Spider-Man Windows

---
+++ Assets/Scripts/Aux Scripts/ReflectionProbeWindow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectionProbeWindow : MonoBehaviour
+{
+    public ReflectionProbe windowProbe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // gameObject.GetComponent<MeshRenderer>().probeAnchor = windowProbe.transform;
+     //   gameObject.GetComponent<MeshRenderer>().material = windowProbe.GetComponent<Renderer>().material;
+       // gameObject.GetComponent<Material>().SetTexture("Window",windowProbe.texture);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

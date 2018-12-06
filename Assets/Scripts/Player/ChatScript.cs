44c08dffcd1ca9919b40ac2e5c17f2c216c25d00
Jack Malvey <john.malvey@mymail.champlain.edu>
2018-12-05 22:58:25 -0500

    chat started

---
+++ Assets/Scripts/Player/ChatScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ChatScript : MonoBehaviour
+{
+    public Text chatBox;
+    public InputField chatInput;
+
+    bool usingChat = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        chatInput.DeactivateInputField();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputCheck();
+        print(chatInput.IsActive());
+        print(usingChat);
+    }
+
+    void inputCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            if (chatInput.GetComponent<Text>())
+                sendChat();
+            toggleChatActivity();
+        }
+        if(usingChat)
+        {
+        }
+    }
+
+    void toggleChatActivity()
+    {
+       
+        usingChat = !usingChat;
+        if (usingChat)
+            chatInput.ActivateInputField();
+        else
+            chatInput.DeactivateInputField();
+    }
+
+    void sendChat()
+    {
+
+    }
+}

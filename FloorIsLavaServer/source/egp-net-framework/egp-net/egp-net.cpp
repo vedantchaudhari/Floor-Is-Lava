c00615d2650acdf08328b92a99fa6272233c2436
Vedant Chaudhari <v.chaudhari@jagoworld.org>
2019-03-13 21:40:03 -0400

    Added RakNet-Unity-Plugin to repo

--- FloorIsLavaServer/source/egp-net-framework/egp-net/egp-net.cpp
+++
@@ -1,9 +0,0 @@
-/*
-	EGP Networking Framework
-	Dan Buckstein
-	October 2018
-
-	Main source file for EGP Networking Framework library.
-*/
-
-#include "egp-net/egp-net.h"
--- FloorIsLavaServer/source/egp-net-game-server/egp-net-game-server-main.cpp
+++
@@ -1,5 +0,0 @@
-
-int main(int const argc, char const *const *const argv)
-{
-
-}
